684dd17b397c7f00949759ad540a75a77ad72138
Leo Graciano <lgraciano@blockitsec.com>
2022-05-16 18:13:18 -0300

    django-on-heroku

--- plataforma/settings/database_settings.py
+++ plataforma/settings/database_settings.py
@@ -5,16 +5,12 @@ from django.conf import settings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if settings.DEBUG:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-        }
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
