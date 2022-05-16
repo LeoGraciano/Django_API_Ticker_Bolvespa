7f3f99fa181b1bf48e6ce8a830c91dd9f51b06cb
Leo Graciano <lgraciano@blockitsec.com>
2022-05-16 19:12:38 -0300

    rediscloud

--- plataforma/settings/celery_settings.py
+++ plataforma/settings/celery_settings.py
@@ -1,12 +1,32 @@
 
+from django.conf  import settings
+import os
 
-CELERY_RESULT_BACKEND = 'django-db'
+if settings.DEBUG:
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
+    CELERY_RESULT_BACKEND = 'django-db'
 
-CELERY_TIMEZONE = 'America/Sao_Paulo'
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
+    CELERY_BROKER_URL = 'redis://localhost:6379'
+    CELERY_ACCEPT_CONTENT = ['application/json']
+    CELERY_RESULT_SERIALIZER = 'json'
+    CELERY_TASK_SERIALIZER = 'json'
+
+    CELERY_TIMEZONE = 'America/Sao_Paulo'
+    CELERY_TASK_TRACK_STARTED = True
+    CELERY_TASK_TIME_LIMIT = 30 * 60
+else:
+    BROKER_URL = BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
+    BROKER_POOL_LIMIT = 1
+    BROKER_CONNECTION_MAX_RETRIES = None
+
+    CELERY_TASK_SERIALIZER = "json"
+    CELERY_ACCEPT_CONTENT = ["json", "msgpack"]
+    CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
+
+    if BROKER_URL == "django://":
+        INSTALLED_APPS += ("kombu.transport.django",)
+
+    BROKER_TRANSPORT_OPTIONS = {
+        "max_connections": 2,
+    }
+    BROKER_POOL_LIMIT = None
