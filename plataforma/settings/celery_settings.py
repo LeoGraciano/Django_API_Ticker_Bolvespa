d7f12e1ca2b78fcbcd9726ab8b37d711f15a61ee
Leo Graciano <lgraciano@blockitsec.com>
2022-05-16 19:41:18 -0300

    celery

--- plataforma/settings/celery_settings.py
+++ plataforma/settings/celery_settings.py
@@ -5,9 +5,9 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
 
-# CELERY_BROKER_URL =  'redis://'
+CELERY_BROKER_URL =  'redis://'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
