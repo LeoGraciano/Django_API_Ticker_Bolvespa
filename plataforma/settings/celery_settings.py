0350250da63e1be3876ca8abaeaaeded8fd58eb7
Leo Graciano <lgraciano@blockitsec.com>
2022-05-16 20:06:22 -0300

    fix error

--- plataforma/settings/celery_settings.py
+++ plataforma/settings/celery_settings.py
@@ -7,7 +7,6 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 
-
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
